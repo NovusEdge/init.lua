76bb001dc488134f86b314d0230899fe6e83a55b
NovusEdge <novusedge0@gmail.com>
2023-07-08 20:17:54 -0400

    Adding tab-completion for lsp-zero plugin (cmp)

--- after/plugin/lspzero.lua
+++ after/plugin/lspzero.lua
@@ -1,5 +1,21 @@
-local lsp = require('lsp-zero').preset({})
-
+local lsp = require('lsp-zero').preset({
+    float_border = 'rounded',
+    call_servers = 'local',
+    configure_diagnostics = true,
+    setup_servers_on_start = true,
+    set_lsp_keymaps = {
+        preserve_mappings = false,
+        omit = {},
+    },
+    manage_nvim_cmp = {
+        set_sources = 'recommended',
+        set_basic_mappings = true,
+        set_extra_mappings = true,
+        use_luasnip = true,
+        set_format = true,
+        documentation_window = true,
+    }
+})
 
 lsp.on_attach(function(client, bufnr)
   	lsp.default_keymaps({buffer = bufnr})
@@ -19,3 +35,37 @@ lsp.ensure_installed({
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local cmp = require('cmp')
+
+-- Straight out of: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#no-snippet-plugin
+cmp.setup({
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = function(fallback)
+            if not cmp.select_next_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+                end
+            end,
+
+        ['<S-Tab>'] = function(fallback)
+            if not cmp.select_prev_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                   cmp.complete()
+                else
+                    fallback()
+                end
+            end
+        end,
+        },
+})
